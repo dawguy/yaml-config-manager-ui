9430ec84416ba4b8704dac30ef742eccabb11196
David Wright <hockeyguy411@gmail.com>
2022-07-02 09:19:50 -0400

    Initial commit, using TUI template from https://github.com/eccentric-j/cljs-tui-template

---
+++ test/yaml_config_manager_ui/core_test.cljs
@@ -0,0 +1,7 @@
+(ns yaml-config-manager-ui.core-test
+  (:require [clojure.test :refer [are deftest is testing]]
+            [yaml-config-manager-ui.core :refer [screen]]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
